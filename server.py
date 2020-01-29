c4283d81b49684e691dfa29cfa3d1263e61ee28a
Viktor Bocz <beviktor95@gmail.com>
2020-01-29 17:24:10 +0100

    Setup basic functions in server

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request, redirect
 
-
 app = Flask(__name__)
 
+counter_dict = {'POST': 0, 'GET': 0, 'DELETE': 0, 'PUT': 0}
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def index():
-    pass
+    counter_dict[request.method] += 1
+    return render_template('index.html')
+
+
+@app.route('/request-counter')
+def request_counter():
+    return render_template('request_counter.html', counter_dict=counter_dict)
+
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html', counter_dict=counter_dict)
 
 
 if __name__ == '__main__':
