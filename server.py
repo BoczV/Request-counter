af10f1906ae6241076e9d257cbca68c52a550ac9
Viktor Bocz <beviktor95@gmail.com>
2020-01-30 11:25:34 +0100

    View layer setup for basic usage, connected with data_manager

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import database_manager
 
 app = Flask(__name__)
-
-counter_dict = {'POST': 0, 'GET': 0, 'DELETE': 0, 'PUT': 0}
 
 
 @app.route('/')
@@ -12,13 +11,14 @@ def index():
 
 @app.route('/request-counter', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def request_counter():
-    counter_dict[request.method] += 1
-    return render_template('request_counter.html', counter_dict=counter_dict)
+    database_manager.insert_new_request_method(request.method)
+    return render_template('request_counter.html')
 
 
 @app.route('/statistics')
 def statistics():
-    return render_template('statistics.html', counter_dict=counter_dict)
+    all_data_from_http = database_manager.get_all_data_from_http()
+    return render_template('statistics.html', all_data_from_http=all_data_from_http)
 
 
 if __name__ == '__main__':
