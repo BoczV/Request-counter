b7b039624397092a90c851338d8b909721c68ca1
Viktor Bocz <beviktor95@gmail.com>
2020-01-30 11:24:27 +0100

    Setup SQL database with tables

--- data/request_counter.sql
+++ data/request_counter.sql
@@ -1,12 +1,24 @@
-CREATE TABLE IF NOT EXISTS HTTP (
-    id INT UNIQUE PRIMARY KEY,
-    request varchar(255),
-    number INT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES USERS (user_id)
-);
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS http CASCADE;
 
-CREATE TABLE IF NOT EXISTS USERS (
+CREATE TABLE IF NOT EXISTS users (
     user_id INT UNIQUE PRIMARY KEY,
     user_name varchar(255)
 );
+
+CREATE TABLE IF NOT EXISTS http (
+    id serial NOT NULL PRIMARY KEY,
+    request varchar(255),
+    number INT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+INSERT INTO http (request, number)
+VALUES ('POST', 0);
+INSERT INTO http (request, number)
+VALUES ('GET', 0);
+INSERT INTO http (request, number)
+VALUES ('PUT', 0);
+INSERT INTO http (request, number)
+VALUES ('DELETE', 0);
